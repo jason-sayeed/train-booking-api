01f6d5907d44c754e42b8408b5a1e51123c52911
Jason <jason.sayeed@gmail.com>
2024-12-22 17:38:19 +0000

    index.ts file updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,12 +18,12 @@ connectToDatabase()
   .catch((error: unknown) => {
     if (error instanceof Error) {
       console.error(
-        'Failed to connect to the database:',
+        'Failed to connect to the db:',
         error.message,
       );
     } else {
       console.error(
-        'Failed to connect to the database. Unknown error:',
+        'Failed to connect to the db. Unknown error:',
         error,
       );
     }
