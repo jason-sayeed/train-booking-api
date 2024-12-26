a26a297c82897fc7eef03be8e48c4fe0a570e1c8
Jason <jason.sayeed@gmail.com>
2024-12-26 20:54:53 +0000

    removed unnecessary comments

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -1,29 +1,23 @@
 import * as dotenv from 'dotenv';
 
-// Load the correct .env file depending on the environment
 const envFile: '.env.test' | '.env' =
   process.env.NODE_ENV === 'test' ? '.env.test' : '.env';
+
 dotenv.config({ path: envFile });
 
 export default {
-  // Use ts-jest preset for testing TypeScript files with Jest
   preset: 'ts-jest',
-  // Set the test environment to Node.js
   testEnvironment: 'node',
   setupFiles: ['dotenv/config'],
 
-  // Define the root directory for tests and modules
   roots: ['<rootDir>/tests'],
 
-  // Use ts-jest to transform TypeScript files
   transform: {
     '^.+\\.(ts|js)$': 'ts-jest',
   },
 
-  // Regular expression to find test files
   testRegex: '((\\.|/)(test|spec))\\.tsx?$',
 
-  // File extensions to recognise in module resolution
   moduleFileExtensions: [
     'ts',
     'tsx',
